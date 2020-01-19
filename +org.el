3d1f8a32fb1f78f9e31345982de3f0f7312fc4a7
Mikhail Levchenko <mishkun.lev@gmail.com>
2020-01-20 01:55:10 +0300

    move config under a vcs

---
+++ +org.el
@@ -0,0 +1,39 @@
+;;; ~/.doom.d/+org.el -*- lexical-binding: t; -*-
+
+;; Org configuration
+
+;; Keywords
+(setq org-todo-keywords '(
+                          ;; General TODOS
+                          (sequence "TODO(t)" "PROJ(p)" "STRT(s)" "WAIT(w)" "|" "DONE(d)" "KILL(k)")
+                          ;; Subtasks
+                          (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")
+                          ;; To-reads
+                          (sequence "TOREAD(r)" "REVIEW(v)" "ARCHIV(a)")))
+
+;; Org-journal setup
+(setq org-journal-file-format
+      "%Y%m%d.org")
+
+;; Org-agenda setup
+(setq org-agenda-files '("~/org" "~/org/projects"))
+(setq org-agenda-custom-commands
+      '(("O" "Overview"
+         ;; TODo's with the A priority are shown on top. these i need to do in first place
+         ((tags-todo "+PRIORITY=\"A\"" ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                                        (org-agenda-overriding-header "High-priority unfinished tasks:")))
+          ;; Calendar agenda for time-bounded tasks
+          (agenda "" ((org-agenda-span 8)
+                      (org-agenda-start-day "-1d")
+                      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))))
+          ;; PR-review requests
+          (todo "TODO" ((org-agenda-files '("~/org/review-pr.org"))
+                      (org-agenda-overriding-header "PR's to review:")))
+          (tags-todo "-PRIORITY=\"A\"-pr-toread" ((org-agenda-overriding-header "Other tasks:")))
+          ))))
+
+;; keybindings
+(map! :leader
+      (:prefix "o"
+        :desc "Org agenda overview" "A" (lambda () "Open my configured agenda view" (interactive) (org-agenda nil "O"))
+        :desc "Org-brain" "B" #'org-brain-visualize))
---
+++ config.el
@@ -0,0 +1,71 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Mikhail Levchenko"
+      user-mail-address "mishkun.lev@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; test
+(setq doom-font (font-spec :family "Iosevka" :size 16)
+      doom-unicode-font (font-spec :family "Iosevka" :size 16)
+      doom-variable-pitch-font (font-spec :family "Helvetica"))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-one)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+;;
+;; MY CONFIG
+;;
+
+;; Startup as fullscreen
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; Set Russian imput method by default
+(load! "+russian.el")
+
+(load! "+org.el")
+
+;; FIXME this is before I figure out how to use .gpg
+(add-to-list 'auth-sources '"~/.authinfo")
+
+;; Plantuml executable configuration to brew
+;;(setq plantuml-executable-path "/usr/local/bin/plantuml")
+;;(setq plantuml-default-exec-mode 'executable)
