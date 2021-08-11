9b064976900ce08679baf2b6e96b6bd710be9a18
Mikhail <mishkun.lev@gmail.com>
2021-08-11 21:11:59 +0300

    Add magit restoration workaround
    Original snippet from persp-mode readme doesn't work, so I replaced
    after-load-function with custom load-function
    
    It appears that persp-mode default :load-function can only save
    file-buffers and will output nil if there is no file parameter. I
    override the whole complex restoration behavior by just calling fresh
    magit-status within buffer directory.

--- config.el
+++ config.el
@@ -101,5 +101,17 @@
       :i "M-0"   (cmd! (insert ")"))
 )
 
+
+; magit restore workaround with persp-mode.el
+(persp-def-buffer-save/load
+ :mode 'magit-status-mode :tag-symbol 'def-magit-status-buffer
+ :save-vars '(default-directory)
+ :load-function #'(lambda (savelist &rest _funcs)
+                      (let* ((vars-list (car (cddr savelist)))
+                             (buf-dir (alist-get 'default-directory vars-list)))
+                        (magit-status buf-dir))
+                    )
+ )
+
 ;; remove outdated default 50 max length
 (setq git-commit-summary-max-length 68)
