f5cd9588e3df5bf64fe57559465ebae0ebc18d45
Mikhail <mishkun.lev@gmail.com>
2021-08-28 18:18:45 +0300

    Install zen mode
    Also remove annoying mixed-pitch and font resizing

--- +appearance.el
+++ +appearance.el
@@ -6,10 +6,20 @@
       doom-variable-pitch-font (font-spec :family "Helvetica"))
 
 ;; Color theme choose
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-one-light)
 
 ;; Absolute line numbers
 (setq display-line-numbers-type t)
 
 ;; Startup as fullscreen
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+
+(after! writeroom-mode
+  (progn
+    ;; disable mixed-pitch and all of the changes to the font altogether
+    (setq +zen-mixed-pitch-modes '())
+    (setq +zen-text-scale 0)
+    (setq writeroom-width 110)
+    (message "Zen loaded with %s %s" +zen-mixed-pitch-modes +zen-text-scale)
+    ))
